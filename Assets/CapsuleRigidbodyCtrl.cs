479b72df45295e8dd7e193cf3a91c6b94e137f30
GaoXingShi <113456881@qq.com>
2020-02-15 22:59:39 +0800

    chore : 星球使用Polybrush插件，Polybrush可以在网格的基础上继续替换网格的形状，并且可以以Terrian的方式放置表面材质与树。材质与树均是Unity商店免费提供的，TerrainTexturesPackFree与Lowpoly风格。

---
+++ Assets/CapsuleRigidbodyCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleRigidbodyCtrl : MonoBehaviour
+{
+    public Transform mCenterObj;
+    private Rigidbody mRigidboyComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRigidboyComponent = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 crossValue = Vector3.Cross(mCenterObj.up, transform.up).normalized;
+        Vector3 northDirect = Vector3.Cross(mCenterObj.up, crossValue).normalized;
+        Debug.DrawLine(transform.position, transform.position+northDirect * 10, Color.red);
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * vertical * 0.03f);
+        transform.Rotate(Vector3.up * horizontal );
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mRigidboyComponent.AddForceAtPosition(-transform.up * 500, mCenterObj.position,ForceMode.Force);
+    }
+}
